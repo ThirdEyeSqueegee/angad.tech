d3520b3124769bad5b09052dc5e70dc0070f3576
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2023-12-19 16:44:26 -0800

    Updates

--- src/components/molecules/ProfileLinks.tsx
+++ src/components/molecules/ProfileLinks.tsx
@@ -2,23 +2,26 @@ import { Button, ButtonGroup, Link } from "@mui/joy";
 import { memo } from "react";
 import { isMobile } from "react-device-detect";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa6";
-import { MdDownload } from "react-icons/md";
+import { MdDownload, MdEmail } from "react-icons/md";
 
 import resume from "../../assets/Angad Misra - Resume.pdf";
 
 export const ProfileLinks = memo(function Links() {
   return (
     <ButtonGroup {...styles.buttonGroup}>
-      <Button href="https://www.linkedin.com/in/angadmisra" startDecorator={<FaLinkedinIn />} {...styles.button}>
-        LinkedIn
+      <Button href="mailto:hello@angad.tech" startDecorator={<MdEmail />} {...styles.button}>
+        Contact
+      </Button>
+      <Button href={resume} startDecorator={<MdDownload />} {...styles.button}>
+        R&eacute;sum&eacute;
       </Button>
       {isMobile ?
         <Button href="https://github.com/ThirdEyeSqueegee" startDecorator={<FaGithub />} {...styles.button}>
           GitHub
         </Button>
       : null}
-      <Button href={resume} startDecorator={<MdDownload />} {...styles.button}>
-        R&eacute;sum&eacute;
+      <Button href="https://www.linkedin.com/in/angadmisra" startDecorator={<FaLinkedinIn />} {...styles.button}>
+        LinkedIn
       </Button>
     </ButtonGroup>
   );
@@ -30,6 +33,7 @@ const styles = {
     sx: { "&:hover,&:active": { transition: "0.25s" } },
   },
   buttonGroup: {
+    orientation: isMobile ? "vertical" : "horizontal",
     sx: { alignSelf: "center", mt: 1 },
   },
 } as const;
